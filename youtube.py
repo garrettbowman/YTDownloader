1ea9b921c742ab2ac693111a39f073c7ede4834b
Gary Bowman <garrettbowman96@gmail.com>
2024-02-01 10:31:52 -0500

    Add open_file_dialog function to allow user to select download folder

--- youtube.py
+++ youtube.py
@@ -9,3 +9,19 @@ def download_video(url, save_path):
         print('Video Downloaded Successfully')
     except Exception as e:
         print(e)
+
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"selected folder:{folder}")
+    return folder
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.withdraw()
+
+    save_path = open_file_dialog()
+
+
+    download_video('https://www.youtube.com/watch?v=9bZkp7q19f0', 'C:/Users/HP/Downloads')
